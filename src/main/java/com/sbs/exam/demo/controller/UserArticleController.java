3837aa1d1b30512638a16e4ca409b4da40ca7825
dbsdlfud <99ysl@naver.com>
2022-07-09 20:17:36 +0900

    게시물 작성로직 중복제거

--- src/main/java/com/sbs/exam/demo/controller/UserArticleController.java
+++ src/main/java/com/sbs/exam/demo/controller/UserArticleController.java
@@ -23,25 +23,29 @@ public class UserArticleController {
 	
 	private void makeTestData() {
 		for (int i = 1; i <= 10; i++) {
-			int id = articlesLastId + 1;
 			String title = "제목 " + i;
 			String body = "내용 " + i;
-			Article article = new Article(id, title, body);
-
-			articles.add(article);
-			articlesLastId = id;
+			
+			writeArticle(title, body);
 		}
 	}
 	
-	@RequestMapping("/user/article/doAdd")
-	@ResponseBody
-	public Article doAdd(String title, String body) {
+	
+	public Article writeArticle(String title, String body) {
 		int id = articlesLastId + 1;
 		Article article =  new Article(id, title, body);
 		
 		articles.add(article);
 		
 		articlesLastId = id;
+		
+		return article;
+	}
+	
+	@RequestMapping("/user/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		Article article = writeArticle(title, body);
 		
 		return article;
 	}

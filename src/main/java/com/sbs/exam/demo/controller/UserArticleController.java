687548862b8581426ba190fe6fe5908da67fa712
dbsdlfud <99ysl@naver.com>
2022-07-10 16:35:53 +0900

    UsrArticleController가 가지고 있던 서비스 로직들을 ArticleService로 옮김

--- src/main/java/com/sbs/exam/demo/controller/UserArticleController.java
+++ src/main/java/com/sbs/exam/demo/controller/UserArticleController.java
@@ -1,6 +1,5 @@
 package com.sbs.exam.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,73 +13,14 @@ import com.sbs.exam.demo.vo.Article;
 @Controller
 public class UserArticleController {
 	
-	
-	// 인스턴스 변수 시작
 	@Autowired
 	private ArticleService articleService;
-	private int articlesLastId;
-	private List<Article> articles;
-	// 인스턴스 변수 끝
-	
-	// 생성자
-	public UserArticleController() {
-		articlesLastId = 0;
-		articles = new ArrayList<>();
-		
-		makeTestData();
-	}
-	
-	// 서비스 메서드 시작
-	private void makeTestData() {
-		for (int i = 1; i <= 10; i++) {
-			String title = "제목 " + i;
-			String body = "내용 " + i;
-			
-			writeArticle(title, body);
-		}
-	}
-	
-	
-	public Article writeArticle(String title, String body) {
-		int id = articlesLastId + 1;
-		Article article =  new Article(id, title, body);
-		
-		articles.add(article);
-		
-		articlesLastId = id;
-		
-		return article;
-	}
-	
-	private Article getArticle(int id) {
-		for ( Article article : articles ) {
-			if ( article.getId() == id ) {
-				return article;
-			}
-		}
 
-		return null;
-	}
-
-	private void deleteArticle(int id) {
-		Article article = getArticle(id);
-
-		articles.remove(article);
-	}
-	
-	private void modifyArticle(int id, String title, String body) {
-		Article article = getArticle(id);
-		
-		article.setTitle(title);
-		article.setBody(body);
-	}
-	// 서비스 메서드 끝
-	
 	// 액션 메서드 시작
 	@RequestMapping("/user/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-		Article article = writeArticle(title, body);
+		Article article = articleService.writeArticle(title, body);
 		
 		return article;
 	}
@@ -88,13 +28,13 @@ public class UserArticleController {
 	@RequestMapping("/user/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
-		return articles;
+		return articleService.getArticles();
 	}
 	
 	@RequestMapping("/user/article/getArticle")
 	@ResponseBody
 	public Object getArticleAction(int id) {
-		Article article = getArticle(id); 
+		Article article = articleService.getArticle(id); 
 		
 		if(article == null) {
 			return id + "번 게시물은 존재하지 않습니다.";
@@ -106,13 +46,13 @@ public class UserArticleController {
 	@ResponseBody
 	public String doDelete(int id) {
 		
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 
 		if ( article == null ) {
 			return id + "번 게시물이 존재하지 않습니다.";			
 		}
 
-		deleteArticle(id);
+		articleService.deleteArticle(id);
 		
 		return id + "번 게시물이 삭제되었습니다.";
 	}
@@ -120,13 +60,13 @@ public class UserArticleController {
 	@RequestMapping("/user/article/doModify")
 	@ResponseBody
 	public String doModify(int id, String title, String body) {
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 		
 		if(article == null) {
 			return id + "번 게시물이 존재하지 않습니다.";
 		}
 		
-		modifyArticle(id, title, body);
+		articleService.modifyArticle(id, title, body);
 		
 		return id + "번 게시물을 수정하였습니다.";
 	}
--- src/main/java/com/sbs/exam/demo/controller/UserHomeController.java
+++ src/main/java/com/sbs/exam/demo/controller/UserHomeController.java
@@ -1,134 +1,16 @@
 package com.sbs.exam.demo.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.sbs.exam.demo.vo.Article;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 @Controller
 public class UserHomeController {
 	
-//	int count;
-//	
-//	public UserHomeController() {
-//		count = 0;
-//	}
-//	
-//	@RequestMapping("/user/home/main")
-//	@ResponseBody
-//	public String showMain() {
-//		return "안녕하세요.";
-//	}
-//	
-//	@RequestMapping("/user/home/main2")
-//	@ResponseBody
-//	public String showMain2() {
-//		return "반갑습니다.";
-//	}
-//	
-//	@RequestMapping("/user/home/main3")
-//	@ResponseBody
-//	public String showMain3() {
-//		return "또 만나요.";
-//	}
-//	
-//	@RequestMapping("/user/home/getCount")
-//	@ResponseBody
-//	public int getCount() {
-//		count++;
-//		return count;
-//	}
-//	
-//	@RequestMapping("/user/home/setCount")
-//	@ResponseBody
-//	public String setCount(int count) {
-//		this.count = count;
-//		return "count 값이 " + this.count + "으로 초기화 되었습니다.";
-//	}
-	
-//	============================================
-	
-//	어떤 타입으로 날려도 결국에는 문장으로 바뀔것이다.
-	@RequestMapping("/user/home/getString")
+	@RequestMapping("/user/home/main")
 	@ResponseBody
-	public String getString() {
-		return "HI";
-	}
-	
-	@RequestMapping("/user/home/getInt")
-	@ResponseBody
-	public int getInt() {
-		return 10;
-	}
-	
-	@RequestMapping("/user/home/getFloat")
-	@ResponseBody
-	public float getFloat() {
-		return 10.5f;
-	}
-	
-	@RequestMapping("/user/home/getDouble")
-	@ResponseBody
-	public double getDouble() {
-		return 10.5;
-	}
-	
-	@RequestMapping("/user/home/getBoolean")
-	@ResponseBody
-	public boolean getBoolean() {
-		return true;
-	}
-	
-	@RequestMapping("/user/home/getChar")
-	@ResponseBody
-	public char getChar() {
-		return 'a';
-	}
-	
-	@RequestMapping("/user/home/getMap")
-	@ResponseBody
-	public HashMap getMap() {
-		HashMap<String, Object> map = new HashMap<>();
-		map.put("철수나이", 22);
-		map.put("영희나이", 21);
-		return map;
-	}
-	
-	@RequestMapping("/user/home/getList")
-	@ResponseBody
-	public List<String> getList() {
-		List<String> list = new ArrayList<>();
-		list.add("쳘수, 영희");
-		return list;
-	}
-	
-	@RequestMapping("/user/home/getArticle")
-	@ResponseBody
-	public Article getArticle() {
-		Article article = new Article(1, "제목 1", "내용1");
-		
-		return article;
-	}
-	
-	@RequestMapping("/user/home/getArticles")
-	@ResponseBody
-	public List<Article> getArticles() {
-		Article article1 = new Article(1, "제목 1", "내용1");
-		Article article2 = new Article(2, "제목 2", "내용2");
-		
-		List<Article> list = new ArrayList<>();
-		list.add(article1);
-		list.add(article2);
-		return list;
+	public String showMain() {
+		return "안녕하세요.";
 	}
 	
 	

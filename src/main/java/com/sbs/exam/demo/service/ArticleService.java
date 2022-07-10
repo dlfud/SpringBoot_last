483728d89202b2f7bafdc5f323f37bbef9eb86d7
dbsdlfud <99ysl@naver.com>
2022-07-10 17:40:35 +0900

    ArticleRepository만듬

--- src/main/java/com/sbs/exam/demo/service/ArticleService.java
+++ src/main/java/com/sbs/exam/demo/service/ArticleService.java
@@ -3,72 +3,40 @@ package com.sbs.exam.demo.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sbs.exam.demo.repository.ArticleRepository;
 import com.sbs.exam.demo.vo.Article;
 
 @Service
 public class ArticleService {
-	// 인스턴스 변수 시작
-		private int articlesLastId;
-		private List<Article> articles;
-		// 인스턴스 변수 끝
-		
-		// 생성자
-		public ArticleService() {
-			articlesLastId = 0;
-			articles = new ArrayList<>();
-			
-			makeTestData();
-		}
-		
-		// 서비스 메서드 시작
-		public void makeTestData() {
-			for (int i = 1; i <= 10; i++) {
-				String title = "제목 " + i;
-				String body = "내용 " + i;
-				
-				writeArticle(title, body);
-			}
-		}
-		
-		
-		public Article writeArticle(String title, String body) {
-			int id = articlesLastId + 1;
-			Article article =  new Article(id, title, body);
-			
-			articles.add(article);
-			
-			articlesLastId = id;
-			
-			return article;
-		}
-		
-		public Article getArticle(int id) {
-			for ( Article article : articles ) {
-				if ( article.getId() == id ) {
-					return article;
-				}
-			}
+//	@Autowired
+	private ArticleRepository articleRepository;
+	
+	//생성자
+	public ArticleService(ArticleRepository articleRepository) {
+		this.articleRepository =  articleRepository;
+	}
 
-			return null;
-		}
+	
+	public Article writeArticle(String title, String body) {
+		return articleRepository.writeArticle(title, body);
+	}
 
-		public void deleteArticle(int id) {
-			Article article = getArticle(id);
+	public Article getArticle(int id) {
+		return articleRepository.getArticle(id);
+	}
 
-			articles.remove(article);
-		}
-		
-		public void modifyArticle(int id, String title, String body) {
-			Article article = getArticle(id);
-			
-			article.setTitle(title);
-			article.setBody(body);
-		}
-		
-		public List<Article> getArticles(){
-			return articles;
-		}
-		// 서비스 메서드 끝
+	public void deleteArticle(int id) {
+		articleRepository.deleteArticle(id);
+	}
+
+	public void modifyArticle(int id, String title, String body) {
+		articleRepository.modifyArticle(id, title, body);
+	}
+
+	public List<Article> getArticles() {
+		return articleRepository.getArticles();
+	}
 }

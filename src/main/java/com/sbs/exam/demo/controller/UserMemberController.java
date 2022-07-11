12f31c512724cecd8e4264472beeb0e22e500645
dbsdlfud <99ysl@naver.com>
2022-07-11 13:59:22 +0900

    회원가입 완료 후, 생성된 회원 보여줌

---
+++ src/main/java/com/sbs/exam/demo/controller/UserMemberController.java
@@ -0,0 +1,30 @@
+package com.sbs.exam.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sbs.exam.demo.service.MemberService;
+import com.sbs.exam.demo.vo.Member;
+
+@Controller
+public class UserMemberController {
+	
+//	@Autowired
+	private MemberService memberService;
+	
+	public UserMemberController(MemberService memberService) {
+		this.memberService = memberService;
+	}
+	
+	@RequestMapping("/user/member/doJoin")
+	@ResponseBody
+	public Member doJoin(String loginId, String loginPw, String name, 
+			String nickname, String cellphoneNo, String email) {
+		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		Member member = memberService.getMemberById(id);
+		return member;
+	}
+	
+	
+}
---
+++ src/main/java/com/sbs/exam/demo/service/MemberService.java
@@ -0,0 +1,27 @@
+package com.sbs.exam.demo.service;
+
+import org.springframework.stereotype.Service;
+
+import com.sbs.exam.demo.repository.MemberRepository;
+import com.sbs.exam.demo.vo.Member;
+
+@Service
+public class MemberService {
+	
+//	@Autowired
+	private MemberRepository memberRepository;
+	
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
+	
+	public int join(String loginId, String loginPw, String name, 
+			String nickname, String cellphoneNo, String email) {
+		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		return memberRepository.getLastInsertId();
+	}
+	
+	public Member getMemberById(int id) {
+		return memberRepository.getMemberById(id);
+	}
+}
